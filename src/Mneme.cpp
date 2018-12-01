49155ea4a524696f85bf2b638852eba324008b93
U-Pixeltron\Gravyman3321 <goldcray@gmail.com>
2018-12-01 14:13:16 -0800

    fixed some things but something's still wrong

--- src/Mneme.cpp
+++ src/Mneme.cpp
@@ -4,7 +4,7 @@
 #define N 4
 #define NIN 3
 #define NOUT 4
-#define BUFL (1<<24)
+#define BUFL (1<<12)
 
 typedef struct
 {
@@ -111,43 +111,43 @@ float buffer_get_tap(cbuf* buffer, float loc)
 }
 
 struct Mneme : Module {
-	enum ParamIds {
+    enum ParamIds {
         ENUMS(DELAY_PARAM,N),
         ENUMS(DELAY_CV_PARAM, N),
         ENUMS(IN_CV_PARAM, N),
         ENUMS(OUT_CV_PARAM, N),
-        ENUMS(FB_CV_PARAM, N*(N-1)),
+        ENUMS(FB_CV_PARAM, N*N),
         NUM_PARAMS
-	};
-	enum InputIds {
+    };
+    enum InputIds {
         ENUMS(DELAY_INPUT, N),
-		ENUMS(SIGNAL_INPUT, N),
-		NUM_INPUTS
-	};
-	enum OutputIds {
+        ENUMS(SIGNAL_INPUT, N),
+        NUM_INPUTS
+    };
+    enum OutputIds {
         ENUMS(SIGNAL_OUTPUT, N),
-		NUM_OUTPUTS
-	};
-	enum LightIds {
-		NUM_LIGHTS
-	};
+        NUM_OUTPUTS
+    };
+    enum LightIds {
+        NUM_LIGHTS
+    };
 
     int ready = 0;
 
-    float pos[N];
-    float vals[N];
+    float pos[N+1];
+    float vals[N+1];
 
     Label* testLabel;
 
     cbuf buffer;
 
-	Mneme() : Module(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS) {}
-	void step() override;
+    Mneme() : Module(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS) {}
+    void step() override;
 
-	// For more advanced Module features, read Rack's engine.hpp header file
-	// - toJson, fromJson: serialization of internal data
-	// - onSampleRateChange: event triggered by a change of sample rate
-	// - onReset, onRandomize, onCreate, onDelete: implements special behavior when user clicks these from the context menu
+    // For more advanced Module features, read Rack's engine.hpp header file
+    // - toJson, fromJson: serialization of internal data
+    // - onSampleRateChange: event triggered by a change of sample rate
+    // - onReset, onRandomize, onCreate, onDelete: implements special behavior when user clicks these from the context menu
 };
 
 void Mneme::step() {
@@ -158,39 +158,42 @@ void Mneme::step() {
     //One big circular buffer
     //Push data into buffer
 
-    buffer_push(&buffer, inputs[SIGNAL_INPUT].value*params[IN_CV_PARAM].value);
+    vals[0] = inputs[SIGNAL_INPUT].value*params[IN_CV_PARAM].value;
+    pos[0] = 0;
+
+    buffer_push(&buffer, vals[0]);
 
     for(int j = 0; j < N; ++j)
     {
-        pos[j] = (inputs[DELAY_INPUT+j].value*params[DELAY_CV_PARAM+j].value)*(BUFL-1)
+        pos[j+1] = (inputs[DELAY_INPUT+j].value*params[DELAY_CV_PARAM+j].value)*(BUFL-1)
                + params[DELAY_PARAM+j].value;
         //maybe clip the length?
         //account for sample rate
 
-        vals[j] = buffer_get_tap(&buffer, pos[j]);
+        vals[j+1] = buffer_get_tap(&buffer, pos[j+1]);
     }
 
-    for(int j = 0; j < N; ++j)
+    for(int j = 0; j < N+1; ++j)
     {
         float accum = 0;
 
         int k = 0; //k tracks the source index
-        for(int i = 0; i < NIN; ++i)
+        for(int i = 0; i < N; ++i)
         {
             if(i==j) ++k;
             else
             {
-                accum += vals[k]*params[FB_CV_PARAM+j*NIN+i].value;
+                accum += vals[k]*params[FB_CV_PARAM+j*N+i].value;
             }
             ++k;
         }
-        if(j != 0)
+        if(j != N)
             accum += inputs[SIGNAL_INPUT+j].value * params[IN_CV_PARAM+j].value;
         //clip signal?
 
         buffer_add_insert(&buffer, pos[j], accum);
-
-        outputs[SIGNAL_OUTPUT+j].value = (accum+vals[j])*params[OUT_CV_PARAM+j].value;
+        if(j>0)
+            outputs[SIGNAL_OUTPUT+j-1].value = (accum+vals[j])*params[OUT_CV_PARAM+j-1].value;
     }
  
      
@@ -208,19 +211,19 @@ struct MnemeWidget : ModuleWidget
 
 
 MnemeWidget::MnemeWidget(Mneme* module) : ModuleWidget(module) {
-	box.size = Vec(20* RACK_GRID_WIDTH, RACK_GRID_HEIGHT);
+    box.size = Vec(20* RACK_GRID_WIDTH, RACK_GRID_HEIGHT);
     
-	{
-		SVGPanel *panel = new SVGPanel();
-		panel->box.size = box.size;
-		panel->setBackground(SVG::load(assetPlugin(plugin, "res/Mneme.svg")));
-		addChild(panel);
-	}
+    {
+        SVGPanel *panel = new SVGPanel();
+        panel->box.size = box.size;
+        panel->setBackground(SVG::load(assetPlugin(plugin, "res/Mneme.svg")));
+        addChild(panel);
+    }
 
-	addChild(Widget::create<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
-	addChild(Widget::create<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
-	addChild(Widget::create<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
-	addChild(Widget::create<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+    addChild(Widget::create<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
+    addChild(Widget::create<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
+    addChild(Widget::create<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+    addChild(Widget::create<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
 
     float xoff, yoff;
@@ -235,12 +238,12 @@ MnemeWidget::MnemeWidget(Mneme* module) : ModuleWidget(module) {
 
         KNOB(xoff+31+6.707,380-(233.164+29), 1, BUFL-1, 10, Huge, Mneme, DELAY_PARAM, j)
 
-        for(int i = 0; i < NIN; ++i)
+        for(int i = 0; i < N; ++i)
         {
 
             yoff = 192.561+12.5 - 38.891*1;
             auto *param = ParamWidget::create<LEDSliderBlue>(
-                Vec(xoff + 13.413+10.63/2 + i*(33.098-13.413), 380-yoff),
+                Vec(xoff + 13.413+10.63/2 + i*(50-10.63)/(N-1), 380-yoff),
                 module, Mneme::FB_CV_PARAM + NIN*j+i,
                 -1, 1, 0
                 );
