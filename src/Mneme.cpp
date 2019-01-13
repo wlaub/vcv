0313e9ec7cc0ffa5bfe4dc7a768ccf23c3332957
wlaub <goldcray@gmail.com>
2019-01-12 17:21:13 -0800

    debugging integration

--- src/Mneme.cpp
+++ src/Mneme.cpp
@@ -6,124 +6,6 @@
 #define NOUT 4
 #define BUFL (1<<16)
 
-typedef struct
-{
-    float data[BUFL];
-    unsigned int head;
-} cbuf;
-
-float interpolate(float left, float right, float alpha)
-{
-    //Buffer interpolate function from left/right values and distance from
-    //left index to value at the corresponding position
-    return left*(1-alpha)+right*(alpha);
-}
-float reverse_interpolate(float val, float alpha, int dir)
-{
-    //Reverse interpolate from value at float position alpha from left index
-    //to value that would appear at left or right index depending on dir
-    if(dir == 0) //left
-    {
-        return val*(1-alpha);
-    }
-    else //right
-    {
-        return alpha*val;
-    }
-}
-
-unsigned int buffer_get_index(cbuf* buffer, unsigned int pos)
-{// printf("buffer_get_index\n");
-    //Return the index of the given position in the past
-//    printf("pos - %i\n", pos);
-//    printf("head - %i\n", buffer->head);
-   
-    while(pos >= BUFL)
-    {
-        pos -= BUFL;
-    }
-    if(pos <= buffer->head)
-    {
-        return buffer->head - pos;
-    }
-    else
-    {
-        if(buffer -> head != BUFL-1)
-            return (BUFL-pos)+buffer->head;
-        else
-            return 0;
-        //pos = head + 1
-        //BUFL-head-1+head = BUFL-1
-        //
-        //pos = BUFL-1
-        //BUFL-BUFL+1=head+1
-    }
-
-}
-
-#define BUF_DECODE(buffer, loc)\
-    float alpha;\
-    unsigned int left, right;\
-    left = buffer_get_index(buffer, floor(loc));\
-    right = left+1; if(right == BUFL) --right;\
-    alpha = loc-floor(loc);\
-
-int buffer_push(cbuf* buffer, float val)
-{// printf("buffer_push\n");
-    buffer->head +=1;
-    if(buffer->head == BUFL) buffer->head = 0;
-    buffer->data[buffer->head] = val;
-
-    return 0;
-}
-
-float buffer_add(cbuf* buffer, int index, float val)
-{
-    //Add the given value at the given index; Clamp as needed
-    buffer->data[index]  += val;
-    buffer->data[index] = clamp(buffer->data[index], -10.f, 10.f);
-
-    return buffer->data[index];
-}
-
-int buffer_insert(cbuf* buffer, float loc, float val)
-{ 
-    //Insert the value at position val into the buffer using reverse
-    //interpolation
-    BUF_DECODE(buffer, loc)
-
-    buffer->data[left]  = reverse_interpolate(val, alpha, 0);
-    buffer->data[right] = reverse_interpolate(val, alpha, 1);
-
-    return 0;
-}
-
-
-int buffer_add_insert(cbuf* buffer, float loc, float val)
-{// printf("buffer_add_insert\n");
-    //Same as insert, but add instead of replace
-    BUF_DECODE(buffer, loc)
-
-    buffer_add(buffer, left, reverse_interpolate(val, alpha, 0));
-//    buffer->data[left] += reverse_interpolate(val, alpha, 1);
-//    buffer->data[left] = clamp(buffer->data[left], -10.f, 10.f);
-    buffer_add(buffer, left, reverse_interpolate(val, alpha, 0));
-//    buffer->data[right] += reverse_interpolate(val, alpha, 1);
-//    buffer->data[right] = clamp(buffer->data[right], -10.f, 10.f);   
-    //Do clipping
-
-    return 0;
-}
-
-float buffer_get_tap(cbuf* buffer, float loc)
-{// printf("buffer_get_tap\n");
-    //Get the value at the given float index
-    BUF_DECODE(buffer, loc)
-
-    return interpolate(buffer->data[left], buffer->data[right], alpha);
-
-}
-
 struct Mneme : Module {
     enum ParamIds {
         ENUMS(DELAY_PARAM,N),
@@ -153,7 +35,7 @@ struct Mneme : Module {
 
     Label* testLabel;
 
-    cbuf buffer;
+    ttt::CircularBuffer* buffer;
 
     Mneme() : Module(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS) {}
     void step() override;
@@ -175,7 +57,7 @@ void Mneme::step() {
     vals[0] = inputs[SIGNAL_INPUT].value*params[IN_CV_PARAM].value;
     pos[0] = 0;
 
-    buffer_push(&buffer, vals[0]);
+    buffer->push(vals[0]);
 
     for(int j = 0; j < N; ++j)
     {
@@ -184,7 +66,7 @@ void Mneme::step() {
         //maybe clip the length?
         //account for sample rate
 
-        vals[j+1] = buffer_get_tap(&buffer, pos[j+1]);
+        vals[j+1] = buffer->get_tap_floating(pos[j+1]);
     }
 
     for(int j = 0; j < N+1; ++j)
@@ -213,11 +95,11 @@ void Mneme::step() {
 
         if(j==0)
         {
-            buffer_add(&buffer, buffer.head, accum);
+            buffer->add(0, accum);
         }
         if(j>0)
         {
-            buffer_add_insert(&buffer, pos[j], accum);
+            buffer->add_floating(pos[j], accum);
             outputs[SIGNAL_OUTPUT+j-1].value = (accum+vals[j])*params[OUT_CV_PARAM+j-1].value;
         }
     }
@@ -251,6 +133,7 @@ MnemeWidget::MnemeWidget(Mneme* module) : ModuleWidget(module) {
     addChild(Widget::create<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
     addChild(Widget::create<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
+    module->buffer = new ttt::CircularBuffer(BUFL);
 
     float xoff, yoff;
 
