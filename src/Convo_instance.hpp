ef9d80fd48748f0d6ffdaeec88f44af121c63326
wlaub <goldcray@gmail.com>
2019-08-10 17:49:57 -0700

    convo update

--- src/Convo_instance.hpp
+++ src/Convo_instance.hpp
@@ -1,7 +1,7 @@
 
 
 // Specify the Module and ModuleWidget subclass, human-readable
-// author name for categorization per pluginInstance, module slug (should never
+// author name for categorization per plugin, module slug (should never
 // change), human-readable module name, and any number of tags
 // (found in `include/tags.hpp`) separated by commas.
 Model *modelConvo = createModel<Convo, ConvoWidget>("Convo");
