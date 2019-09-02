cb9d77fa1bb013753fd38474f27f4553160c28b1
wlaub <goldcray@gmail.com>
2019-09-01 21:26:40 -0700

    Tap finding in prometheus

--- src/Prometheus2.cpp
+++ src/Prometheus2.cpp
@@ -1,6 +1,13 @@
 
 #include "TechTechTechnologies.hpp"
 
+typedef struct {
+    unsigned short left;
+    unsigned short _;
+    unsigned short right;
+    unsigned short value;
+
+} IndexEntry;
 
 struct Prometheus2 : Module {
     /* +ENUMS */
@@ -12,8 +19,7 @@ struct Prometheus2 : Module {
     /* -TRIGGER_VARS */
 
     unsigned char* raw_index[2];
-    unsigned int* master_index[2];
-    unsigned short* data_index[2];
+    unsigned short get_taps(unsigned short length, unsigned short param0, unsigned short param1, unsigned char order);
 
     Prometheus2()
     {
@@ -38,6 +44,11 @@ struct Prometheus2 : Module {
                 fread(raw_index[i], 1, length, fp);
 
                 fclose(fp);
+
+                get_taps(1, 30000, 30000, i);
+                get_taps(11, 30000, 30000, i);
+                get_taps(4095, 30000, 30000, i);
+
             }
             else
             {
@@ -53,6 +64,81 @@ struct Prometheus2 : Module {
     // - onSampleRateChange: event triggered by a change of sample rate
     // - onReset, onRandomize, onCreate, onDelete: implements special behavior when user clicks these from the context menu
 };
+
+unsigned short Prometheus2::get_taps(unsigned short length, unsigned short param0, unsigned short param1, unsigned char order)
+{
+    unsigned char* buffer = raw_index[order];
+    unsigned int base_address = ((unsigned int*)(buffer))[length];
+
+
+    unsigned short actual_size = *(unsigned short*)(buffer+base_address-2);
+
+//printf("Length %i maps to actual length %i starting at address %i\n", length, actual_size, base_address);
+
+    unsigned short left0 = 0;
+    unsigned short right0 = *(unsigned short*)(buffer+base_address+2);
+    unsigned short index0 = 0;
+
+//printf("There are %i options available here\n", right0);
+
+    //I know there is a better way to do this, but I don't have bw to care rn.
+    while(1)
+    {
+        IndexEntry* entry0 = (IndexEntry*)(buffer+base_address+index0*4);
+        printf("%i ?? %i ?? %i\n", entry0->left, param0, entry0->right);
+        printf("=%i/%i/%i\n", left0, index0, right0);
+
+        if(param0 < entry0->left)
+        {
+            right0=index0;
+            index0 = (index0+left0)/2;
+        }
+        else if(param0 >= entry0->right)
+        {
+            left0=index0;
+            index0 = (index0+right0)/2;
+        }
+        else
+        {
+            unsigned int sub_address = base_address+*(unsigned short*)(buffer+base_address+6+index0*4);
+            unsigned short left1 = 0;
+            unsigned short right1 = *(unsigned short*)(buffer+sub_address+2);
+            unsigned short index1 = 0;
+
+//printf("  There are %i options available here\n", right1);
+
+           
+            unsigned int count = 0;
+            while(count < 100)
+            {
+                count += 1;
+                IndexEntry* entry1 = (IndexEntry*)(buffer+sub_address+index1*4);
+//printf("  %i ?? %i ?? %i\n", entry1->left, param1, entry1->right);
+//printf("  =%i/%i/%i\n", left1, index1, right1);
+                if(param1 < entry1->left)
+                {
+                    right1 = index1;
+                    index1 = (index1+left1)/2;
+                }
+                else if(param1 >= entry1->right)
+                {
+                    left1 = index1;
+                    index1 = (index1+right1)/2;
+                }
+                else
+                {
+//printf("<----%x\n", entry1->value);
+                    return entry1->value;
+                }
+            }
+            return 0;
+            
+        }
+    }
+
+    
+}
+
 
 
 void Prometheus2::step() {
