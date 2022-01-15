d20385a3aa7ebc19f51d6e3ae440f8f8c7349177
wlaub <goldcray@gmail.com>
2022-01-15 14:54:40 -0800

    fix convo

--- src/Convo.cpp
+++ src/Convo.cpp
@@ -11,7 +11,7 @@ struct Convo : Module {
     #include "Convo_vars.hpp"
     /* -TRIGGER_VARS */
 
-    SchmittTrigger clock_trigger[2];
+    dsp::SchmittTrigger clock_trigger[2];
 
     int ready = 0;
 
@@ -29,7 +29,84 @@ struct Convo : Module {
 		config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
         #include "Convo_paramconfig.hpp"       
     }
-    void step() override;
+
+    void process(const ProcessArgs& args) override {
+        float deltaTime = args.sampleTime;
+
+        if(ready == 0) return;
+        /*  +INPUT_PROCESSING */
+        #include "Convo_inputs.hpp"
+        /*  -INPUT_PROCESSING */
+
+        for(int i = 0; i < 2; ++i)
+        {
+            //push signal buffer
+            if(inputs[INPUT_SIGNAL_IN+i].active)
+            {
+                signal[i]->push(input_signal_in[i]);        
+            }
+            else
+            {
+                signal[i]->push(0);
+            }
+
+            //Process kernel clock and runst
+
+            if(param_runst[i] == 1)
+            {
+                float smpl_voct = 1*param_freq_offset[i];
+                float smpl_freq = 261.626f * powf(2.0f, smpl_voct);
+
+                smpl_phase[i] += deltaTime*smpl_freq;
+                int int_clock = 0;
+                if(smpl_phase[i] > 1)
+                {
+                    smpl_phase[i] -= 1;
+                    int_clock=1;
+                }    
+
+                if(inputs[INPUT_CLOCK_IN+i].active)
+                {
+                    auto gate = clock_trigger[i].process(input_clock_in[i]);
+                    if(gate)
+                    {
+                        kernel[i]->push(input_kernel_in[i]*param_kernel_gain[i]/10.);
+                    }
+                }
+                else
+                {   //Process internal kernel clock
+                    if(int_clock == 1)
+                    {
+                         kernel[i]->push(input_kernel_in[i]*param_kernel_gain[i]/10.);                   
+                    }
+                }
+            }
+
+            //Process window shape
+
+            double result = 0;
+            for(int j = 0; j < BUFL; ++j)
+            {
+               float w = .9/((abs(j*param_window_rollof[i]-BUFL*param_window_length[i])+1));
+               //Compute convolution
+               result += w*signal[i]->get_tap(j)*kernel[i]->get_tap(j);
+            }
+
+            output_conv_out[i] = result;
+
+            //Write output
+
+        }
+
+        /*  +OUTPUT_PROCESSING */
+        #include "Convo_outputs.hpp"
+        /*  -OUTPUT_PROCESSING */
+
+
+
+    }
+
+
 
     // For more advanced Module features, read Rack's engine.hpp header file
     // - dataToJson, dataFromJson: serialization of internal data
@@ -37,82 +114,6 @@ struct Convo : Module {
     // - onReset, onRandomize, onCreate, onDelete: implements special behavior when user clicks these from the context menu
 };
 
-
-void Convo::step() {
-    float deltaTime = engineGetSampleTime();
-
-    if(ready == 0) return;
-    /*  +INPUT_PROCESSING */
-    #include "Convo_inputs.hpp"
-    /*  -INPUT_PROCESSING */
-
-    for(int i = 0; i < 2; ++i)
-    {
-        //push signal buffer
-        if(inputs[INPUT_SIGNAL_IN+i].active)
-        {
-            signal[i]->push(input_signal_in[i]);        
-        }
-        else
-        {
-            signal[i]->push(0);
-        }
-
-        //Process kernel clock and runst
-
-        if(param_runst[i] == 1)
-        {
-            float smpl_voct = 1*param_freq_offset[i];
-            float smpl_freq = 261.626f * powf(2.0f, smpl_voct);
-
-            smpl_phase[i] += deltaTime*smpl_freq;
-            int int_clock = 0;
-            if(smpl_phase[i] > 1)
-            {
-                smpl_phase[i] -= 1;
-                int_clock=1;
-            }    
-
-            if(inputs[INPUT_CLOCK_IN+i].active)
-            {
-                auto gate = clock_trigger[i].process(input_clock_in[i]);
-                if(gate)
-                {
-                    kernel[i]->push(input_kernel_in[i]*param_kernel_gain[i]/10.);
-                }
-            }
-            else
-            {   //Process internal kernel clock
-                if(int_clock == 1)
-                {
-                     kernel[i]->push(input_kernel_in[i]*param_kernel_gain[i]/10.);                   
-                }
-            }
-        }
-
-        //Process window shape
-
-        double result = 0;
-        for(int j = 0; j < BUFL; ++j)
-        {
-           float w = .9/((abs(j*param_window_rollof[i]-BUFL*param_window_length[i])+1));
-           //Compute convolution
-           result += w*signal[i]->get_tap(j)*kernel[i]->get_tap(j);
-        }
-
-        output_conv_out[i] = result;
-
-        //Write output
-
-    }
-
-    /*  +OUTPUT_PROCESSING */
-    #include "Convo_outputs.hpp"
-    /*  -OUTPUT_PROCESSING */
-
-
-
-}
 
 
 struct ConvoWidget : ModuleWidget {
