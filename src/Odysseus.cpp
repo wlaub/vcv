41be176c8bad553b79dc1666dcf8a87603cfbc31
wlaub <goldcray@gmail.com>
2022-01-15 14:56:20 -0800

    fixed odysseus

--- src/Odysseus.cpp
+++ src/Odysseus.cpp
@@ -58,8 +58,8 @@ struct Odysseus : Module {
     float integrator = 0;
     float shphase = 0;
 
-    SchmittTrigger clearTrigger;
-    SchmittTrigger clkTrigger;
+    dsp::SchmittTrigger clearTrigger;
+    dsp::SchmittTrigger clkTrigger;
 
 	Odysseus() 
     {
@@ -83,189 +83,191 @@ struct Odysseus : Module {
     }
 	void step() override;
 
+    float curve_lut(float val, float lut[][2], int len)
+    {
+        if(val <= lut[0][0]) return lut[0][1];
+        if(val >= lut[len-1][0]) return lut[len-1][1];
+        int i = 0;
+        for (i = 0; val >= lut[i][0]; ++i);
+        float* next = lut[i];
+        float* prev = lut[i-1];
+
+        float a = (val-prev[0])/(next[0]-prev[0]);
+        return (1-a)*prev[1] + a*next[1];
+
+    }
+
+    float curve_pot(float a, float res, float rt=-1, float rb=-1, float rtx = 0, float rbx = 0)
+    {/*
+        return divider ratio of a curved pot
+        a is turn amount. left to right -> 0 to 1
+        res is pot resistance
+        rt is top parallel resistance. rt < 0 indicates none
+        rb is bottom parallel resitance. rb < 0 indicates none
+        rtx and rtb are series top and bottom resistors
+        */
+
+        float rta, rba;
+        rba = a*res;
+        rta = (1-a)*res;
+        if(rt >= 0) rta = rt*rta/(rt+rta);
+        if(rb >= 0) rba = rb*rba/(rb+rba);
+        rta += rtx;
+        rba += rbx;
+
+        return rba/(rta+rba);
+    }
+
+    void process(const ProcessArgs& args) override {
+        float deltaTime = args.sampleTime;
+
+        if(ready == 0) return;
+
+        //Offset level
+        float off_cv = inputs[OFFSET_INPUT].value * 
+            2*(curve_pot(params[OFFSET_CV_PARAM].value, 100, 9.31, 9.31)-.5);
+        float off_knob = 
+            10*2*(curve_pot(params[OFFSET_PARAM].value, 100, 6.98, 6.98, 3.09, 3.09)-.5);
+
+        float off_val = CLAMP6011(off_cv + off_knob);
+
+        //Noise level
+        float noise_cv = inputs[NOISE_INPUT].value * 
+            curve_pot(params[NOISE_CV_PARAM].value, 100, -1, 13.3);
+        float noise_knob = 
+            5*curve_pot(params[NOISE_PARAM].value, 100, -1, 10);
+
+        float noise_level = 5.36-(noise_cv + noise_knob);
+        float std_dev = NOISE_STD_DEV*curve_lut(noise_level, NOISE_LUT, NOISE_LUT_LEN);
+
+        //Generate noise
+       
+        float noise_val = 0;
+        if(std_dev > 0 )
+        {
+            noise_val = CLAMP6011(std_dev*random::normal());
+        }
+
+        float sig_val = CLAMP6011(noise_val+off_val);
+
+        //Sample and hold clock
+        float freq = 1000;
+        int ndiv = 1;
+
+        if(params[FS_PARAM].value > 0.5) //fast mode = 64
+        {
+            ndiv = 64;
+        }
+        else
+        {
+            if(params[SS_PARAM].value > 0.5) //slow mode = 1024
+            {
+                ndiv = 1024;
+            }
+            else //slower mode = 16384
+            {
+                ndiv = 16384;
+            }
+        }
+
+        float freq_knob = 1000*(1-params[FREQ_PARAM].value);
+        freq_knob = freq_knob*3e3/(freq_knob+3e3);
+        freq_knob += 49.9; 
+
+        freq = 1e6 * 50 / (freq_knob*ndiv);
+
+        shphase += freq*deltaTime;
+
+        float clk_sig = 0;
+
+        while(shphase >= 1)
+        {
+            shphase -= 1;
+            clk_sig = 5;
+        }
+
+
+        if(inputs[CLK_INPUT].active)
+        {
+            clk_sig = inputs[CLK_INPUT].value;
+        }
+        else
+        {
+        }
+
+        clkTrigger.process(clk_sig);
+
+        //Sample and hold on signal
+
+        if (clkTrigger.isHigh())
+        {
+            sh_val = sig_val;
+        }
+
+        //Rate attenuation
+       
+        float rate_knob = 
+        (curve_pot(params[RATE_PARAM].value, 100, -1, 1.15));
+
+        float int_in_val = sh_val*rate_knob;
+
+        //Integrate
+
+        integrator = CLAMP2057(integrator+int_in_val*INT_RATE);
+
+        //Clear/autoclear etc
+
+        float clear_sig= 0;
+
+        if (params[AC_PARAM].value >0.5)
+        {
+            clear_sig += outputs[DETECT_OUTPUT].value;
+        }
+
+        clear_sig += 10*params[CLEAR_PARAM].value;
+        clear_sig += inputs[CLEAR_INPUT].value;
+
+        clearTrigger.process(clear_sig/CLEAR_THRESH);
+
+        if(clearTrigger.isHigh())
+        {
+            integrator = 0;
+        }
+
+        float out = integrator;
+
+        //Detect
+
+        float detect_knob =
+        (curve_pot(params[DETECT_PARAM].value, 100, -1, 6.98));
+        
+        if (abs(detect_knob*out) >= DETECT_THRESH)
+        {
+            outputs[DETECT_OUTPUT].value = 5;
+        }
+        else
+        {
+            outputs[DETECT_OUTPUT].value = 0;
+        }
+
+        outputs[OUT_OUTPUT].value = CLAMP6011(out+inputs[IN_INPUT].value);
+
+        /*
+        char tstr[256];
+        sprintf(tstr, "freq: %f", freq);
+        if(testLabel)
+            testLabel->text = tstr;
+        */
+    }
+
+
+
 	// For more advanced Module features, read Rack's engine.hpp header file
 	// - dataToJson, dataFromJson: serialization of internal data
 	// - onSampleRateChange: event triggered by a change of sample rate
 	// - onReset, onRandomize, onCreate, onDelete: implements special behavior when user clicks these from the context menu
 
 };
-
-float curve_lut(float val, float lut[][2], int len)
-{
-    if(val <= lut[0][0]) return lut[0][1];
-    if(val >= lut[len-1][0]) return lut[len-1][1];
-    int i = 0;
-    for (i = 0; val >= lut[i][0]; ++i);
-    float* next = lut[i];
-    float* prev = lut[i-1];
-
-    float a = (val-prev[0])/(next[0]-prev[0]);
-    return (1-a)*prev[1] + a*next[1];
-
-}
-
-float curve_pot(float a, float res, float rt=-1, float rb=-1, float rtx = 0, float rbx = 0)
-{/*
-    return divider ratio of a curved pot
-    a is turn amount. left to right -> 0 to 1
-    res is pot resistance
-    rt is top parallel resistance. rt < 0 indicates none
-    rb is bottom parallel resitance. rb < 0 indicates none
-    rtx and rtb are series top and bottom resistors
-    */
-
-    float rta, rba;
-    rba = a*res;
-    rta = (1-a)*res;
-    if(rt >= 0) rta = rt*rta/(rt+rta);
-    if(rb >= 0) rba = rb*rba/(rb+rba);
-    rta += rtx;
-    rba += rbx;
-
-    return rba/(rta+rba);
-}
-
-void Odysseus::step() {
-	float deltaTime = 1.0 / engineGetSampleRate();
-
-    if(ready == 0) return;
-
-    //Offset level
-    float off_cv = inputs[OFFSET_INPUT].value * 
-        2*(curve_pot(params[OFFSET_CV_PARAM].value, 100, 9.31, 9.31)-.5);
-    float off_knob = 
-        10*2*(curve_pot(params[OFFSET_PARAM].value, 100, 6.98, 6.98, 3.09, 3.09)-.5);
-
-    float off_val = CLAMP6011(off_cv + off_knob);
-
-    //Noise level
-    float noise_cv = inputs[NOISE_INPUT].value * 
-        curve_pot(params[NOISE_CV_PARAM].value, 100, -1, 13.3);
-    float noise_knob = 
-        5*curve_pot(params[NOISE_PARAM].value, 100, -1, 10);
-
-    float noise_level = 5.36-(noise_cv + noise_knob);
-    float std_dev = NOISE_STD_DEV*curve_lut(noise_level, NOISE_LUT, NOISE_LUT_LEN);
-
-    //Generate noise
-   
-    float noise_val = 0;
-    if(std_dev > 0 )
-    {
-        noise_val = CLAMP6011(std_dev*randomNormal());
-    }
-
-    float sig_val = CLAMP6011(noise_val+off_val);
-
-    //Sample and hold clock
-    float freq = 1000;
-    int ndiv = 1;
-
-    if(params[FS_PARAM].value > 0.5) //fast mode = 64
-    {
-        ndiv = 64;
-    }
-    else
-    {
-        if(params[SS_PARAM].value > 0.5) //slow mode = 1024
-        {
-            ndiv = 1024;
-        }
-        else //slower mode = 16384
-        {
-            ndiv = 16384;
-        }
-    }
-
-    float freq_knob = 1000*(1-params[FREQ_PARAM].value);
-    freq_knob = freq_knob*3e3/(freq_knob+3e3);
-    freq_knob += 49.9; 
-
-    freq = 1e6 * 50 / (freq_knob*ndiv);
-
-    shphase += freq*deltaTime;
-
-    float clk_sig = 0;
-
-    while(shphase >= 1)
-    {
-        shphase -= 1;
-        clk_sig = 5;
-    }
-
-
-    if(inputs[CLK_INPUT].active)
-    {
-        clk_sig = inputs[CLK_INPUT].value;
-    }
-    else
-    {
-    }
-
-    clkTrigger.process(clk_sig);
-
-    //Sample and hold on signal
-
-    if (clkTrigger.isHigh())
-    {
-        sh_val = sig_val;
-    }
-
-    //Rate attenuation
-   
-    float rate_knob = 
-    (curve_pot(params[RATE_PARAM].value, 100, -1, 1.15));
-
-    float int_in_val = sh_val*rate_knob;
-
-    //Integrate
-
-    integrator = CLAMP2057(integrator+int_in_val*INT_RATE);
-
-    //Clear/autoclear etc
-
-    float clear_sig= 0;
-
-    if (params[AC_PARAM].value >0.5)
-    {
-        clear_sig += outputs[DETECT_OUTPUT].value;
-    }
-
-    clear_sig += 10*params[CLEAR_PARAM].value;
-    clear_sig += inputs[CLEAR_INPUT].value;
-
-    clearTrigger.process(clear_sig/CLEAR_THRESH);
-
-    if(clearTrigger.isHigh())
-    {
-        integrator = 0;
-    }
-
-    float out = integrator;
-
-    //Detect
-
-    float detect_knob =
-    (curve_pot(params[DETECT_PARAM].value, 100, -1, 6.98));
-    
-    if (abs(detect_knob*out) >= DETECT_THRESH)
-    {
-        outputs[DETECT_OUTPUT].value = 5;
-    }
-    else
-    {
-        outputs[DETECT_OUTPUT].value = 0;
-    }
-
-    outputs[OUT_OUTPUT].value = CLAMP6011(out+inputs[IN_INPUT].value);
-
-    /*
-    char tstr[256];
-    sprintf(tstr, "freq: %f", freq);
-    if(testLabel)
-        testLabel->text = tstr;
-    */
-}
 
 struct OdysseusWidget : ModuleWidget
 {
