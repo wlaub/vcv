6e7b826e287f7879df0da7bfc8af99479cd93399
wlaub <goldcray@gmail.com>
2022-01-16 18:24:28 -0800

    implemented order knob and cable detections

--- src/Polyphemus2.cpp
+++ src/Polyphemus2.cpp
@@ -1,5 +1,24 @@
 #include "TechTechTechnologies.hpp"
 
+//The cutoff frequency gain
+#define Hc .707
+//The maximum pole order
+#define Nmax 10
+
+
+
+struct PoleFilter {
+
+    double mem = 0;
+
+    double get_y(double x, double r, double k)
+    {
+        double y = x*k+r*mem;
+        mem = y;
+        return y;
+    }
+
+};
 
 struct Polyphemus2 : Module {
 	enum ParamIds {
@@ -7,7 +26,7 @@ struct Polyphemus2 : Module {
 		BIAS1_PARAM,
 		BIAS2_PARAM,
 		BIAS3_PARAM,
-		ENVGAIN0_PARAM,
+		ORDER_PARAM,
 		ENVGAIN1_PARAM,
 		ENVGAIN2_PARAM,
 		ENVGAIN3_PARAM,
@@ -73,14 +92,16 @@ struct Polyphemus2 : Module {
 
     float buffer[4][3];
 
+    struct PoleFilter filters[4][3][Nmax];
+
 	Polyphemus2() {
 		config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
         for(int i = 0; i < 4; ++i)
         {
             configParam(BIAS0_PARAM+i, -1.f, 1.f, 0.f, "Envelope bias offset");
-            configParam(ENVGAIN0_PARAM+i, 0.f, 1.f, 0.f, "Not Implemented");
+            configParam(ORDER_PARAM+i, 1.f, Nmax , 1.f, "Pole Order");
             configParam(ENVP0_PARAM+i, 0.f, 1.f, 1.f, "Envelope level");
-            configParam(GAIN0_PARAM+i, 0.f, 19.f, 9.f, "Frequency gain above knee");
+            configParam(GAIN0_PARAM+i, 0.f, 19.f, 1.f, "Frequency gain above knee");
             configParam(KNEE0_PARAM+i, 0.f, 1.f, 0.5f, "Cutoff frequency knee level");
             configParam(VOCTP0_PARAM+i, -3.f, 3.f, 0.f, "Base cutoff Frequency");
             for(int j = 0; j < 3; ++ j)
@@ -90,12 +111,24 @@ struct Polyphemus2 : Module {
         }
 	}
 
-    float get_r(float wc, float kc=0.5)
+    double get_k(double r, double N)
+    {
+        return 1-r;
+        return pow(1-r, floor(N));
+    }
+
+    double get_gain(double w, double r)
+    {
+        return 1/sqrt(r*r-2*r*cos(w)+1);
+    }
+
+
+    double get_r(double wc, double N)
     {
         //Kc must be 0-1 gain^2 at cutoff frequency 
-        kc = 1/kc;
-        float a = 1-kc; //a = c
-        float b = 2*(kc-cos(wc));
+        double kc = 1/pow(Hc, 2.f/floor(N));
+        double a = 1-kc; //a = c
+        double b = 2*(kc-cos(wc));
         if(b > 2*a)
         {
             return (-b + sqrt(b*b-4*a*a))/(2*a);
@@ -106,52 +139,72 @@ struct Polyphemus2 : Module {
         }
     }
 
-    float get_k(float w, float r)
-    {
-        return 1/sqrt(r*r-2*r*cos(w)+1);
-    }
-
 
 	void process(const ProcessArgs &args) override {
 
         for(int i = 0; i < 4; ++i)
         {
-            float t = 1.f;
-            float knee = params[KNEE0_PARAM+i].getValue();
-            
-            if(inputs[ENV0_INPUT+i].active)
-            {
-                t = inputs[ENV0_INPUT+i].getVoltage()/10.f;
-            }
-            t *= params[ENVP0_PARAM+i].getValue();
-            t += params[BIAS0_PARAM+i].getValue();
-            if(t <= knee) 
-            {
-                t /= knee;
-            }
-            else
-            {
-                t = 1+(t-knee)/(1-knee)*params[GAIN0_PARAM+i].getValue();
-            }
 
-            float pitch = params[VOCTP0_PARAM+i].getValue();
-            pitch += inputs[VOCT0_INPUT+i].getVoltage();
-            float freq =  dsp::FREQ_C4 * std::pow(2.f, pitch) * t;
-            float wc = 6.28*freq*args.sampleTime;
-            wc = clamp(wc, 0.f, 3.14f);
-
-            float r = get_r(wc);
-            float k = get_k(wc, r);
-
-//            printf("%f: %f, %f\n", wc, r, k);
+            bool pane_active = false;
             for(int j = 0; j < 3; ++j)
             {
-                float x = inputs[IN00_INPUT+i+4*j].getVoltage();
-                float y = x/k+r*buffer[i][j];
-                buffer[i][j] = y;
-                outputs[OUT00_OUTPUT+i+4*j].setVoltage(y);
+                pane_active |= inputs[IN00_INPUT+i+4*j].active;
             }
+    
+            if(pane_active)
+            {
+                float t = 1.f;
+                float knee = params[KNEE0_PARAM+i].getValue();
+                
+                if(inputs[ENV0_INPUT+i].active)
+                {
+                    t = inputs[ENV0_INPUT+i].getVoltage()/10.f;
+                }
+                t *= params[ENVP0_PARAM+i].getValue();
+                t += params[BIAS0_PARAM+i].getValue();
+                if(t <= knee) 
+                {
+                    t /= knee;
+                }
+                else
+                {
+                    t = 1+(t-knee)/(1-knee)*params[GAIN0_PARAM+i].getValue();
+                }
 
+                float pitch = params[VOCTP0_PARAM+i].getValue();
+                pitch += inputs[VOCT0_INPUT+i].getVoltage();
+                float freq =  dsp::FREQ_C4 * std::pow(2.f, pitch) * t;
+                float wc = 6.28*freq*args.sampleTime;
+                wc = clamp(wc, 0.f, 3.14f);
+
+                float N = params[ORDER_PARAM+i].getValue();
+
+                double r = get_r(wc, N);
+                double k = get_k(r, N);
+
+    //TODO: Check for presence of cables
+    //            printf("%f: %f, %f\n", wc, r, k);
+                for(int j = 0; j < 3; ++j)
+                {
+                    float y = 0;
+                    if(inputs[IN00_INPUT+i+4*j].active)
+                    {
+                        float x = inputs[IN00_INPUT+i+4*j].getVoltage();
+
+                        for(int n = 0; n < Nmax; ++n)
+                        {
+                            x = filters[i][j][n].get_y(x,r,k);
+                            if(n == (floor(N) -1))
+                            {
+                                y = x;
+                            }
+                        }
+                    }
+    //                float y = x/k+r*buffer[i][j];
+    //                buffer[i][j] = y;
+                    outputs[OUT00_OUTPUT+i+4*j].setVoltage(y);
+                }
+            }
         }
 
 	}
@@ -169,30 +222,54 @@ struct Polyphemus2Widget : ModuleWidget {
 		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
-        addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(17.3, 48.274)), module, Polyphemus2::BIAS0_PARAM));
-        addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(58.958, 48.274)), module, Polyphemus2::BIAS1_PARAM));
-        addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(100.616, 48.274)), module, Polyphemus2::BIAS2_PARAM));
-        addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(142.274, 48.274)), module, Polyphemus2::BIAS3_PARAM));
-        addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(32.003, 48.274)), module, Polyphemus2::ENVGAIN0_PARAM));
-        addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(73.661, 48.274)), module, Polyphemus2::ENVGAIN1_PARAM));
-        addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(115.319, 48.274)), module, Polyphemus2::ENVGAIN2_PARAM));
-        addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(156.977, 48.274)), module, Polyphemus2::ENVGAIN3_PARAM));
-        addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(32.003, 33.571)), module, Polyphemus2::ENVP0_PARAM));
-        addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(73.661, 33.571)), module, Polyphemus2::ENVP1_PARAM));
-        addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(115.319, 33.571)), module, Polyphemus2::ENVP2_PARAM));
-        addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(156.977, 33.571)), module, Polyphemus2::ENVP3_PARAM));
-        addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(32.003, 62.977)), module, Polyphemus2::GAIN0_PARAM));
-        addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(73.661, 62.977)), module, Polyphemus2::GAIN1_PARAM));
-        addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(115.319, 62.977)), module, Polyphemus2::GAIN2_PARAM));
-        addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(156.977, 62.977)), module, Polyphemus2::GAIN3_PARAM));
-        addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(17.3, 62.977)), module, Polyphemus2::KNEE0_PARAM));
-        addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(58.958, 62.977)), module, Polyphemus2::KNEE1_PARAM));
-        addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(100.616, 62.977)), module, Polyphemus2::KNEE2_PARAM));
-        addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(142.274, 62.977)), module, Polyphemus2::KNEE3_PARAM));
-        addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(17.3, 33.571)), module, Polyphemus2::VOCTP0_PARAM));
-        addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(58.958, 33.571)), module, Polyphemus2::VOCTP1_PARAM));
-        addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(100.616, 33.571)), module, Polyphemus2::VOCTP2_PARAM));
-        addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(142.274, 33.571)), module, Polyphemus2::VOCTP3_PARAM));
+        addParam(createParamCentered<RoundBlackKnob>(
+            mm2px(Vec(17.3, 48.274)), module, Polyphemus2::BIAS0_PARAM));
+        addParam(createParamCentered<RoundBlackKnob>(
+            mm2px(Vec(58.958, 48.274)), module, Polyphemus2::BIAS1_PARAM));
+        addParam(createParamCentered<RoundBlackKnob>(
+            mm2px(Vec(100.616, 48.274)), module, Polyphemus2::BIAS2_PARAM));
+        addParam(createParamCentered<RoundBlackKnob>(
+            mm2px(Vec(142.274, 48.274)), module, Polyphemus2::BIAS3_PARAM));
+        addParam(createParamCentered<RoundBlackSnapKnob>(
+            mm2px(Vec(32.003, 48.274)), module, Polyphemus2::ORDER_PARAM));
+        addParam(createParamCentered<RoundBlackKnob>(
+            mm2px(Vec(73.661, 48.274)), module, Polyphemus2::ENVGAIN1_PARAM));
+        addParam(createParamCentered<RoundBlackKnob>(
+            mm2px(Vec(115.319, 48.274)), module, Polyphemus2::ENVGAIN2_PARAM));
+        addParam(createParamCentered<RoundBlackKnob>(
+            mm2px(Vec(156.977, 48.274)), module, Polyphemus2::ENVGAIN3_PARAM));
+        addParam(createParamCentered<RoundBlackKnob>(
+            mm2px(Vec(32.003, 33.571)), module, Polyphemus2::ENVP0_PARAM));
+        addParam(createParamCentered<RoundBlackKnob>(
+            mm2px(Vec(73.661, 33.571)), module, Polyphemus2::ENVP1_PARAM));
+        addParam(createParamCentered<RoundBlackKnob>(
+            mm2px(Vec(115.319, 33.571)), module, Polyphemus2::ENVP2_PARAM));
+        addParam(createParamCentered<RoundBlackKnob>(
+            mm2px(Vec(156.977, 33.571)), module, Polyphemus2::ENVP3_PARAM));
+        addParam(createParamCentered<RoundBlackKnob>(
+            mm2px(Vec(32.003, 62.977)), module, Polyphemus2::GAIN0_PARAM));
+        addParam(createParamCentered<RoundBlackKnob>(
+            mm2px(Vec(73.661, 62.977)), module, Polyphemus2::GAIN1_PARAM));
+        addParam(createParamCentered<RoundBlackKnob>(
+            mm2px(Vec(115.319, 62.977)), module, Polyphemus2::GAIN2_PARAM));
+        addParam(createParamCentered<RoundBlackKnob>(
+            mm2px(Vec(156.977, 62.977)), module, Polyphemus2::GAIN3_PARAM));
+        addParam(createParamCentered<RoundBlackKnob>(
+            mm2px(Vec(17.3, 62.977)), module, Polyphemus2::KNEE0_PARAM));
+        addParam(createParamCentered<RoundBlackKnob>(
+            mm2px(Vec(58.958, 62.977)), module, Polyphemus2::KNEE1_PARAM));
+        addParam(createParamCentered<RoundBlackKnob>(
+            mm2px(Vec(100.616, 62.977)), module, Polyphemus2::KNEE2_PARAM));
+        addParam(createParamCentered<RoundBlackKnob>(
+            mm2px(Vec(142.274, 62.977)), module, Polyphemus2::KNEE3_PARAM));
+        addParam(createParamCentered<RoundBlackKnob>(
+            mm2px(Vec(17.3, 33.571)), module, Polyphemus2::VOCTP0_PARAM));
+        addParam(createParamCentered<RoundBlackKnob>(
+            mm2px(Vec(58.958, 33.571)), module, Polyphemus2::VOCTP1_PARAM));
+        addParam(createParamCentered<RoundBlackKnob>(
+            mm2px(Vec(100.616, 33.571)), module, Polyphemus2::VOCTP2_PARAM));
+        addParam(createParamCentered<RoundBlackKnob>(
+            mm2px(Vec(142.274, 33.571)), module, Polyphemus2::VOCTP3_PARAM));
 
         addInput(createInputCentered<PJ301MPort>(mm2px(Vec(32.003, 18.869)), module, Polyphemus2::ENV0_INPUT));
         addInput(createInputCentered<PJ301MPort>(mm2px(Vec(73.661, 18.869)), module, Polyphemus2::ENV1_INPUT));
