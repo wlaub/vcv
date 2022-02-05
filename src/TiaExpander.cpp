5ec83f00cee710dd862a3d92f32ad1a13bcc05fa
wlaub <goldcray@gmail.com>
2022-02-04 18:06:16 -0800

    panel for tia expander

---
+++ src/TiaExpander.cpp
@@ -0,0 +1,77 @@
+#include "TechTechTechnologies.hpp"
+
+#define N 7
+#define IDX(row, col) row*N+col
+
+struct TiaIExpander : Module {
+    enum ParamId {
+       PARAMS_LEN
+    };
+    enum InputId {
+        SIGNAL_INPUT,
+        INPUTS_LEN = SIGNAL_INPUT + 7*N
+    };
+    enum OutputId {
+        POLY_OUTPUT,
+        OUTPUTS_LEN = POLY_OUTPUT + N
+    };
+    enum LightId {
+        LIGHTS_LEN
+    };
+
+
+    TiaIExpander() {
+        config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
+
+        for(int row = 0; row < N; ++row)
+        {
+            for(int col = 0; col < 7; ++col)
+            {
+                configInput(SIGNAL_INPUT+IDX(row, col), "Source");
+            }
+
+            configOutput(POLY_OUTPUT+row, "Crossfader Outputs (Polyphonic)");
+        }
+    }
+
+    void process(const ProcessArgs& args) override {
+
+    }
+
+};
+
+
+struct TiaIExpanderWidget : ModuleWidget {
+
+
+
+    TiaIExpanderWidget(TiaIExpander* module) {
+        setModule(module);
+        setPanel(createPanel(asset::plugin(pluginInstance, "res/Tia.svg")));
+
+        addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
+        addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
+        addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+        addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+
+
+        #define GRID(x,y) 15.24*(x-0.5), 15.24*(y)+3.28
+
+        for(int row = 0; row < N; ++row)
+        {
+            for(int col = 0; col < 7; ++col)
+            {
+
+                addInput(createInputCentered<PJ301MPort>(
+                        mm2px(Vec(GRID(col+1, row+1))), module, TiaIExpander::SIGNAL_INPUT+IDX(row,col)));
+         
+            }
+
+            addOutput(createOutputCentered<PJ301MPort>(
+                    mm2px(Vec(GRID(8,row+1))), module, TiaIExpander::POLY_OUTPUT+row));
+        }
+    }
+};
+
+
+Model* modelTiaIExpander = createModel<TiaIExpander, TiaIExpanderWidget>("TiaIExpander");
